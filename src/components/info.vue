d88b4c115574430f211ca76ef9027768db9b4ac9
CRUX56 <designers_crux@live.com>
2022-11-28 14:04:42 -0500

    Initial commit

---
+++ src/components/info.vue
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-3">
+                    <h3 class="text-muted">About</h3>
+                    <p>
+                        CRUX offers web design, web development, illustration, and graphic design services managed by our senior designer / developer Dathan Cruz
+                    </p>
+                </div>
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6">
+                    <h3 class="text-muted">Contact</h3>
+                    <p>
+                        If you have a scheduling inquiry for our creative services or would wish to chat for an upcoming project need, please contact us at <a href="mailto:">changethisemailaddress@email.com</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
