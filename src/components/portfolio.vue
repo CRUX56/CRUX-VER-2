35850f9b9eceae3b48ad0bafec6af8c389c8ab82
Dathan Cruz <designers_crux@live.com>
2023-04-27 18:45:14 -0400

    Created portfolio component

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -4,10 +4,53 @@
 
 <template>
     <section class="section bg-dark">
-        <div class="container">
-            <div class="row justify-content-center">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
                 <div class="portfolio-item">
-                    
+                    <div class="text-center mb-5">
+                        <router-link :to="{ name: 'Portfolio-test-item-1'}">
+                            <div class="thumb-hover">
+                                <p>Portfolio Test 1</p>
+                            </div>
+                            <img src="" alt="">
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="portfolio-item">
+                    <div class="text-center mb-5">
+                        <router-link :to="{ name: 'Portfolio-test-item-2'}">
+                            <div class="thumb-hover">
+                                <p>Portfolio Test 2</p>
+                            </div>
+                            <img src="" alt="">
+                        </router-link>
+                    </div>
+                </div>    
+            </div>
+            <div class="col-lg-6">
+                <div class="portfolio-item">
+                    <div class="text-center mb-5">
+                        <router-link :to="{ name: 'Portfolio-test-item-3'}">
+                            <div class="thumb-hover">
+                                <p>Portfolio Test 3</p>
+                            </div>
+                            <img src="" alt="" />
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="portfolio-item">
+                    <div class="text-center mb-5">
+                        <router-link :to="{ name: 'Portfolio-test-item-4'}">
+                            <div class="thumb-hover">
+                                <p>Portfolio Test 4</p>
+                            </div>
+                            <img src="" alt=""/>
+                        </router-link>
+                    </div>
                 </div>
             </div>
         </div>
